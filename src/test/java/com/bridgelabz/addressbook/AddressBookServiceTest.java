e6d802f9eb02f1a92cee043d15966df6c96470f4
Kaynat Zehra <kaynatzehra23@gmail.com>
2022-03-28 18:06:30 +0530

    Added: Address Book Problem UC19

--- src/test/java/com/bridgelabz/addressbook/AddressBookServiceTest.java
+++ src/test/java/com/bridgelabz/addressbook/AddressBookServiceTest.java
@@ -1,7 +1,9 @@
 package com.bridgelabz.addressbook;
 import org.junit.jupiter.api.Test;
+
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import static com.bridgelabz.addressbook.AddressBookService.IOService.DB_IO;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,14 +13,14 @@ class AddressBookServiceTest
     public void givenAddressBookInDB_WhenRetrieved_ShouldMatchThePeopleCount() {
         AddressBookService addressBookService = new AddressBookService();
         List<AddressBookData> addressBookDataList = addressBookService.readAddressBookData(DB_IO);
-        assertEquals(5,addressBookDataList.size());
+        assertEquals(7,addressBookDataList.size());
     }
     @Test
     public void givenNewPhoneNumber_ShouldUpdateTheRecordAndSyncWithDataBase() throws AddressBookException {
         AddressBookService addressBookService = new AddressBookService();
         addressBookService.readAddressBookData(DB_IO);
-        addressBookService.updateRecord("Kaynat", "9967587648");
-        boolean result = addressBookService.checkRecordSyncWithDB("Kaynat");
+        addressBookService.updateRecord("Payal", " 8796564738");
+        boolean result = addressBookService.checkRecordSyncWithDB("Payal");
         assertTrue(result);
     }
     @Test
@@ -28,6 +30,24 @@ class AddressBookServiceTest
         LocalDate startDate = LocalDate.of(2018, 1, 1);
         LocalDate endDate = LocalDate.now();
         List< AddressBookData> employeePayrollData= addressBookService.readEmployeePayrollForDateRange(DB_IO, startDate, endDate);
-        assertEquals(0,employeePayrollData.size());
+        assertEquals(1,employeePayrollData.size());
+    }
+    @Test
+    public void givenCity_ShouldRetrieveTheNumberOfContacts_BasedOnCity() {
+        AddressBookService addressBookService = new AddressBookService();
+        addressBookService.readAddressBookData(DB_IO);
+        Map<String, Double> contactsByCity = addressBookService.contactsByCity(DB_IO);
+        System.out.println(contactsByCity.containsKey("Noida")+" "+contactsByCity.containsValue(2.0));
+        assertTrue(contactsByCity.containsKey("Noida") && contactsByCity.containsValue(2.0));
+    }
+
+    @Test
+    public void givenCity_ShouldRetrieveTheNumberOfContacts_BasedOnState() {
+        AddressBookService addressBookService = new AddressBookService();
+        addressBookService.readAddressBookData(DB_IO);
+        Map<String, Double> contactsByState = addressBookService.contactsByState(DB_IO);
+        System.out.println(contactsByState.containsKey("UP")+" "+contactsByState.containsValue(2.0));
+        assertTrue(contactsByState.containsKey("UP") &&
+                contactsByState.containsValue(2.0));
     }
 }
